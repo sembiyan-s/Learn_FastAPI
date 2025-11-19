4ea593155e1964258d78a156ff0d25a6486ca7d1
sembiyan-s <sembiyan01@gmail.com>
2025-11-19 10:50:42 +0530

    Update FastApi_App.py

--- FastApi_App.py
+++ FastApi_App.py
@@ -23,4 +23,4 @@ def info():
     return {"app": "Web App", "version": "1.0.0"}
 
 if __name__ == "__main__":
-   uvicorn.run(app, host="127.0.0.1", port=8000)
+   uvicorn.run(app, host="0.0.0.0", port=8000)
